f0c8ae5f59c45bb7a12d463d862c757637ce5258
Hunter Rogers <hunterwr1997@gmail.com>
2025-02-03 14:52:40 -0700

    added tree assets

--- blender_trees.py
+++ blender_trees.py
@@ -18,97 +18,21 @@ def create_pine_tree(name, position=(0, 0, 0), height=7, trunk_radius=0.2):
     :param height: Height of the tree.
     :param trunk_radius: Radius of the tree trunk.
     """
-    bpy.ops.curve.tree_add(
-    do_update=True, 
-    bevel=True, 
-    prune=False, 
-    showLeaves=False, 
-    useArm=False, 
-    seed=2, 
-    handleType='0', 
-    levels=2, 
-    length=(0.8, 0.6, 0.5, 0.1), 
-    lengthV=(0, 0.1, 0, 0), 
-    taperCrown=0.5, 
-    branches=(0, 55, 10, 1), 
-    curveRes=(8, 5, 3, 1), 
-    curve=(0, -15, 0, 0), 
-    curveV=(20, 50, 75, 0), 
-    curveBack=(0, 0, 0, 0), 
-    baseSplits=3, 
-    segSplits=(0.1, 0.5, 0.2, 0), 
-    splitByLen=True, 
-    rMode='rotate', 
-    splitAngle=(18, 18, 22, 0), 
-    splitAngleV=(5, 5, 5, 0), 
-    scale=5, scaleV=2, 
-    attractUp=(3.5, -1.89984, 0, 0), 
-    attractOut=(0, 0.8, 0, 0), 
-    shape='7', shapeS='10', 
-    customShape=(0.5, 1, 0.3, 0.5), 
-    branchDist=1.5, nrings=0, 
-    baseSize=0.3, 
-    baseSize_s=0.16, 
-    splitHeight=0.2, 
-    splitBias=0.55, 
-    ratio=0.015, 
-    minRadius=0.0015, 
-    closeTip=False, 
-    rootFlare=1, 
-    autoTaper=True, 
-    taper=(1, 1, 1, 1), 
-    radiusTweak=(1, 1, 1, 1), 
-    ratioPower=1.2, 
-    downAngle=(0, 26.21, 52.56, 30), 
-    downAngleV=(0, 10, 10, 10), 
-    useOldDownAngle=True, 
-    useParentAngle=True, 
-    rotate=(99.5, 137.5, 137.5, 137.5), 
-    rotateV=(15, 0, 0, 0), 
-    scale0=1, 
-    scaleV0=0.1, 
-    pruneWidth=0.34, 
-    pruneBase=0.12, 
-    pruneWidthPeak=0.5, 
-    prunePowerHigh=0.5,
-    prunePowerLow=0.001, 
-    pruneRatio=0.75, 
-    leaves=150, 
-    leafDownAngle=30, 
-    leafDownAngleV=-10, 
-    leafRotate=137.5, 
-    leafRotateV=15, 
-    leafScale=0.4, 
-    leafScaleX=0.2, 
-    leafScaleT=0.1, 
-    leafScaleV=0.15, 
-    leafShape='hex', 
-    bend=0, 
-    leafangle=-12, 
-    horzLeaves=True, 
-    leafDist='6', 
-    bevelRes=1, 
-    resU=4, 
-    armAnim=False, 
-    previewArm=False, 
-    leafAnim=False, 
-    frameRate=1, 
-    loopFrames=0, 
-    wind=1, 
-    gust=1, 
-    gustF=0.075, 
-    af1=1, 
-    af2=1, 
-    af3=4, 
-    makeMesh=False, 
-    armLevels=2, 
-    boneStep=(1, 1, 1, 1))
+    bpy.ops.curve.tree_add(do_update=True, chooseSet='5', bevel=True, prune=False, showLeaves=True, useArm=False, seed=0, handleType='0', levels=4, length=(1, 0.2, 0.55, 0.45), lengthV=(0, 0.1, 0.25, 0.25), taperCrown=0, branches=(0, 75, 16, 10), curveRes=(12, 4, 3, 2), curve=(0, 20, -10, 0), curveV=(40, 30, 15, 10), curveBack=(0, 0, 0, 0), baseSplits=0, segSplits=(0, 0.35, 0.6, 0), splitByLen=True, rMode='rotate', splitAngle=(0, 12, 18, 0), splitAngleV=(0, 0, 0, 0), scale=35, scaleV=15, attractUp=(0, 0.35, 0.25, 0.15), attractOut=(0, 0, 0, 0), shape='8', shapeS='7', customShape=(0.5, 1, 0.35, 0.1), branchDist=1.85, nrings=0, baseSize=0.33, baseSize_s=0.5, splitHeight=0.2, splitBias=0, ratio=0.015, minRadius=0.002, closeTip=True, rootFlare=1.3, autoTaper=True, taper=(1, 1, 1, 1), radiusTweak=(1, 1, 1, 1), ratioPower=1.2, downAngle=(90, 130, 45, 45), downAngleV=(0, 30, 10, 10), useOldDownAngle=False, useParentAngle=True, rotate=(99.5, 137.5, -45, -60), rotateV=(15, 0, 15, 45), scale0=1, scaleV0=0.1, pruneWidth=0.4, pruneBase=0.3, pruneWidthPeak=0.6, prunePowerHigh=0.5, prunePowerLow=0.001, pruneRatio=1, leaves=25, leafDownAngle=45, leafDownAngleV=10, leafRotate=137.5, leafRotateV=0, leafScale=1, leafScaleX=1, leafScaleT=0, leafScaleV=0, leafShape='rect', bend=0, leafangle=0, horzLeaves=True, leafDist='6', bevelRes=1, resU=4, armAnim=False, previewArm=False, leafAnim=False, frameRate=1, loopFrames=0, wind=1, gust=1, gustF=0.075, af1=1, af2=1, af3=4, makeMesh=False, armLevels=2, boneStep=(1, 1, 1, 1))
     
-    obj = bpy.context.object  # Get the newly created object
-    obj.name = name  # Rename the object
+    bpy.data.objects["tree"].name = name
     
     select_object(name)
     move_selected_object(position)
+    
+    # selecet the leaves
+    bpy.ops.material.new()
+    bpy.ops.image.open(filepath="C:\\Users\\Hunter Rogers\\Desktop\\Personal\\University of Utah\\DEep cap\\CS6953Project\\assets\\fir_branch_02_by_nellygraceng-d9lcsgk.png", directory="C:\\Users\\Hunter Rogers\\Desktop\\Personal\\University of Utah\\DEep cap\\CS6953Project\\assets\\", files=[{"name":"fir_branch_02_by_nellygraceng-d9lcsgk.png", "name":"fir_branch_02_by_nellygraceng-d9lcsgk.png"}], show_multiview=False)
+    bpy.ops.node.link(detach=False, drag_start=(-30.6074, 201.239))
+    # select tree trunk
+    bpy.ops.object.convert(target='MESH')
+    bpy.ops.material.new()
+    bpy.ops.node.nw_add_textures_for_principled(filepath="C:\\Users\\Hunter Rogers\\Desktop\\Personal\\University of Utah\\DEep cap\\CS6953Project\\assets\\Bark014_8K-JPG\\Bark014_8K-JPG_NormalGL.jpg", directory="C:\\Users\\Hunter Rogers\\Desktop\\Personal\\University of Utah\\DEep cap\\CS6953Project\\assets\\Bark014_8K-JPG\\", files=[{"name":"Bark014.png", "name":"Bark014.png"}, {"name":"Bark014_8K-JPG_NormalGL.jpg", "name":"Bark014_8K-JPG_NormalGL.jpg"}, {"name":"Bark014_8K-JPG_Color.jpg", "name":"Bark014_8K-JPG_Color.jpg"}], relative_path=True)
     
 def move_selected_object(position):
     bpy.ops.transform.translate(value=position, 
@@ -212,9 +136,11 @@ bpy.ops.object.select_all(action='SELECT')  # Select all objects
 bpy.ops.object.delete()  # Delete selected objects
 
 create_pine_tree("tree1", position=(0,0,0)) #
+
 create_pine_tree("tree2", position=(0,2,0))
 create_pine_tree("tree3", position=(2,0,0))
 create_pine_tree("tree4", position=(0,0,2))
+
 
 
 #Hi
